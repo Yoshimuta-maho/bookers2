002ed421a76ee548dfe55189f661363e4cb5b967
EC2 Default User <ec2-user@ip-172-31-4-147.ap-northeast-1.compute.internal>
2025-01-25 05:50:13 +0000

    20250125-14:50

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Book was successfully created."
+      flash[:notice] = "Welcome! You have signed up successfully."
       redirect_to user_path(@user)
     else
       @user = User.all
       flash.now[:notice] = "errors prohibited this book from being saved:"
-      render :edit
+      render 'homes/top'
     end
   end
   def edit
@@ -30,15 +32,21 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     if user.destroy
-      flash[:notice] = "ログアウトしました"
+      flash[:notice] = "Signed out successfully."
       redirect_to 'homes/top'
     else 
       @user = User.allocate
       flash.now[:notice] = "ログアウトに失敗しました"
       render :users_path
     end
-    
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to users_path(user.id)
+  end
+
 
   
   private
